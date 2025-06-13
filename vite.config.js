d54572eb30a65c89c56e51cf122ce4af09938b23
luiserb <68169211+luiserb@users.noreply.github.com>
2025-06-13 12:12:33 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'https://luiserb.github.io/claseacademy-frontend/',
+  base: '/claseacademy-frontend/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
